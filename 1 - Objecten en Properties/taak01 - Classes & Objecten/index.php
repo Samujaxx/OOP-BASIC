3e5b863a22aedd0ebec1ce3dd86b64947f3ffe12
samujaxx <samuraijax2003@gmail.com>
2022-09-12 09:50:00 +0200

    v1.2

---
+++ 1 - Objecten en Properties/taak01 - Classes & Objecten/index.php
@@ -0,0 +1,14 @@
+<?php
+
+class Auto{
+  public $merk;
+  public $model;
+  public $bouwjaar;
+  public $brandstof;
+  public $cilinders;
+  public $ciliderInhoud;
+  public $kleur;
+}
+
+$nieuweAuto = new Auto();
+$$nogEenAndereAuto = new Auto();
---
+++ 1 - Objecten en Properties/taak02 - Properties/index.php
@@ -0,0 +1,30 @@
+<?php
+
+class Auto{
+  public $merk;
+  public $model;
+  public $bouwjaar;
+  public $brandstof;
+  public $cilinders;
+  public $ciliderInhoud;
+  public $kleur;
+}
+
+$nieuweAuto = new Auto();
+$nieuweAuto->merk = "Dodge";
+$nieuweAuto->model = "Hennesey Demon";
+$nieuweAuto->bouwjaar = "2018";
+$nieuweAuto->brandstof = "Benzine";
+$nieuweAuto->cilinders = "8";
+$nieuweAuto->cilinderInhoud = "6.1";
+$nieuweAuto->kleur = "rood";
+
+
+$nogEenAndereAuto = new Auto();
+$nogEenAndereAuto->merk = "Dodge";
+$nogEenAndereAuto->model = "Hennesey Demon";
+$nogEenAndereAuto->bouwjaar = "2018";
+$nogEenAndereAuto->brandstof = "Benzine";
+$nogEenAndereAuto->cilinders = "8";
+$nogEenAndereAuto->cilinderInhoud = "6.1";
+$nogEenAndereAuto->kleur = "rood";
